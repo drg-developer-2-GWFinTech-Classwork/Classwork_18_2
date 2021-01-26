6a08f12628fafa48831251f259d4a2e5461ccfe4
David G <davidrgayman@gmail.com>
2021-01-25 19:19:17 -0500

    Completed activity 3.

--- 03-Stu_Hashlib/Unsolved/hashing.py
+++ 03-Stu_Hashlib/Unsolved/hashing.py
@@ -2,30 +2,33 @@ import hashlib
 
 # output sha256 hash in hexadecimal string format
 def hash(message):
+    # return hashlib.md5(message).hexdigest()
     return hashlib.sha256(message).hexdigest()
 
 # modify these messages
 # note: we include the "b" before the string definition in order to represent it as bytes instead of a string
-message_one = b""
+message_one = b"This is a super-secret message."
 
-message_two = b""
+message_two = b"This is a super-secret message. The bird is in the hen-house."
 
 # print both messages and their corresponding hashes
-print()
-print()
+message_one_hash = hash(message_one)
+message_two_hash = hash(message_two)
+print(f"{message_one} ({message_one_hash})")
+print(f"{message_two} ({message_two_hash})")
 
 # compare the hashes in an if/else statement
 
-hash_one =
+hash_one = message_one_hash
 
-hash_two =
+hash_two = message_two_hash
 
-if ():
-    print()
+if (hash_one == hash_two):
+    print("The hashes are equivalent.")
 else:
-    print()
+    print("The hashes are NOT equivalent.")
 
 # compare the length of the hashes
 
-print(len())
-print(len())
+print(len(hash_one))
+print(len(hash_two))
